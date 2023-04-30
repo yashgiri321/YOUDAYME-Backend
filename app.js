a422e200f48e8d3e49e2d3fcbc47450fef488d7d
yashgiri321 <90254077+yashgiri321@users.noreply.github.com>
2023-05-01 02:26:23 +0530

    payload

--- app.js
+++ app.js
@@ -10,12 +10,12 @@ config({
 
 const app=express();
 
-app.use(express.json({
-  limit: '50mb'
-}));
+
 
 //using Middleware
-app.use(express.json());
+app.use(express.json({
+    limit: '50mb'
+}));
 app.use(express.urlencoded({
     extended: true,
 })
